95b831951b7a3fe3d52b94f92404360e7d12409f
Achilleas Koutsou <achilleas.k@gmail.com>
2017-02-19 22:55:41 +0100

    [avatar] Split out avatar to separate file

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -48,51 +45,6 @@ func makeHTML(content []byte) []byte {
 	return rendered.Bytes()
 }
 
-type gravatarJSON struct {
-	Entry []struct {
-		ID                string `json:"id"`
-		Hash              string `json:"hash"`
-		RequestHash       string `json:"requestHash"`
-		ProfileURL        string `json:"profileUrl"`
-		PreferredUsername string `json:"preferredUsername"`
-		ThumbnailURL      string `json:"thumbnailUrl"`
-		Photos            []struct {
-			Value string `json:"value"`
-			Type  string `json:"type"`
-		} `json:"photos"`
-		Name struct {
-			GivenName string `json:"givenName"`
-		} `json:"name"`
-		DisplayName string        `json:"displayName"`
-		Urls        []interface{} `json:"urls"`
-	} `json:"entry"`
-}
-
-func gravatar(username string) []byte {
-	fmt.Println("Fetching Gravatar")
-	gravURL := fmt.Sprintf("https://en.gravatar.com/%s.json", url.PathEscape(username))
-	res, err := http.Get(gravURL)
-	checkError(err)
-	b, err := ioutil.ReadAll(res.Body)
-	checkError(err)
-
-	var grav gravatarJSON
-	err = json.Unmarshal(b, &grav)
-	checkError(err)
-
-	imgURL := grav.Entry[0].Photos[0].Value
-	imgsize := 160
-	imgURL = fmt.Sprintf("%s?s=%d", imgURL, imgsize)
-	fmt.Printf("Downloading image [%s]\n", imgURL)
-	res, err = http.Get(imgURL)
-	checkError(err)
-
-	imgbytes, err := ioutil.ReadAll(res.Body)
-	checkError(err)
-
-	return imgbytes
-}
-
 func filenameNoExt(fname string) string {
 	fname = filepath.Base(fname)
 	return strings.TrimSuffix(fname, filepath.Ext(fname))
@@ -128,21 +80,6 @@ func createDirs(conf map[string]interface{}) {
 	if !os.IsExist(err) {
 		checkError(err)
 	}
-}
-
-func getAvatar(conf map[string]interface{}) {
-	imgpath := filepath.Join(conf["imagepath"].(string), "avatar.jpg")
-
-	if _, err := os.Stat(imgpath); os.IsNotExist(err) {
-		gravatarUser := conf["gravatarusername"].(string)
-		if gravatarUser != "" {
-			avatar := gravatar(gravatarUser)
-			fmt.Printf("Saving to %s\n", imgpath)
-			err = ioutil.WriteFile(imgpath, avatar, 0666)
-			checkError(err)
-		}
-	}
-	fmt.Printf("Found profile picture [%s]. Will not download.\n", imgpath)
 }
 
 func main() {
